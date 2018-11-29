0c9665873a0f93984c03c93c7c09f94ffabb8306
Diego Martinelli <martinelli.diego@gmail.com>
2018-11-29 17:22:33 +0100

    hackerrank interview kit: 2d array - ds

---
+++ hackerrank/interview-kit/arrays/2d-arrays/hourglasssum.js
@@ -0,0 +1,25 @@
+function hourglassSum(h) {
+    let max = -Infinity
+
+    for (let i = 1; i <= 4; i++) {
+        for (let j = 1; j <= 4; j++) {
+            let count = 0
+            let near = [
+                [i-1, j-1], [i-1,j], [i-1,j+1],
+                [i,j],
+                [i+1, j-1], [i+1, j], [i+1, j+1]
+            ]
+
+            for (let n of near) {
+                count += h[n[0]][n[1]]
+            }
+
+            max = Math.max(max, count)
+        }
+    }
+
+    return max
+}
+
+module.exports = hourglassSum
+
