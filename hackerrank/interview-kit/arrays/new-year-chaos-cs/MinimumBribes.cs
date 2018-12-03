89c3c4654ee0f212ae19d36ee4b8a4365ec50dbb
Diego Martinelli <martinelli.diego@gmail.com>
2018-12-03 11:17:17 +0100

    hackerrank interview kit: new year chaos (c#, but still times out :-( )

---
+++ hackerrank/interview-kit/arrays/new-year-chaos-cs/MinimumBribes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace bribes {
+    public class TooChaoticException : Exception {
+        public TooChaoticException() {}
+        public TooChaoticException(string message)
+            : base (message) {}
+    }
+
+    public class MinimumBribes {
+        private const int MAX_BRIBES = 2;
+
+        public int GetBribesCount(int[] queue) {
+            var N = queue.Length;
+            var bribes = 0;
+
+            for (var i = 0; i < N; i++) {
+                if (queue[i] - MAX_BRIBES - 1 > i)
+                    throw new TooChaoticException("Too Chaotic");
+
+                var partialBribes = 0;
+                for (var j = i + 1; j < N; j++) {
+                    if (queue[j] < queue[i])
+                        partialBribes++;
+                    if (partialBribes >= MAX_BRIBES)
+                        break;
+                }
+                bribes += partialBribes;
+            }
+
+            return bribes;
+        }
+    }
+}
